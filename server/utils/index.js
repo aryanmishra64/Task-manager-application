2e9a213bf8abc54e467fe3b2869c165c9a69581c
aryanmishra64 <127666009+aryanmishra64@users.noreply.github.com>
2024-09-02 01:24:45 +0530

    Update index.js

--- server/utils/index.js
+++ server/utils/index.js
@@ -23,9 +23,9 @@ export const createJWT = (res, userId) => {
  
  
   res.cookie("token", token, {
-   httpOnly: true,
-    secure: process.env.NODE_ENV === "production" ? true : false,
-    sameSite: "none",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production", 
+  sameSite: process.env.NODE_ENV === "production" ? "lax" : "strict", 
+  maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
